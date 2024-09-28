8061b74706db1d7df76161cfdb73cd74eae3736b
Inés Lorenzo <info.ineslorenzo@gmail.com>
2024-09-28 17:19:13 +0200

    primer commit

---
+++ src/model.ts
@@ -0,0 +1,10 @@
+interface Partida{
+    puntuacionUsuario: number;
+}
+
+export const partida: Partida = {
+    puntuacionUsuario: 0,
+}
+
+export type Carta = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 10 | 11 | 12;
+
